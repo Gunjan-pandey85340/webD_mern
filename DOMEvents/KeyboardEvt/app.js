63e23cccd1fd441e3bfd217cdbd655a243be5425
Gunjan Pandey <122886879+Gunjan-pandey85340@users.noreply.github.com>
2025-06-25 17:10:17 +0530

    Add files via upload

---
+++ DOMEvents/KeyboardEvt/app.js
@@ -0,0 +1,10 @@
+let inp = document.querySelector("input");
+// inp.addEventListener("keydown",function () {
+//     console.log("key was pressed");
+    
+// })
+
+inp.addEventListener("keyup",function () {
+    console.log("key was released");
+    
+})
